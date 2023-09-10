1702aefd122ccc92a3da6f0ff922fddd5a2035ca
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-09-10 02:14:22 -0700

    add a lib folder

--- bin/index.js
+++ bin/index.js
@@ -1,24 +1,24 @@
 #! /usr/bin/env node
 
 import inquirer from "inquirer";
-import path from "path";
-import { fileURLToPath } from "url";
+import getFiles from "../lib/fileReader.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = process.cwd();
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "my list",
-      message: "select from list",
-      choices: ["react", "vue", "angular", "jquery", "svelte"],
-    },
-  ])
-  .then((answers) => {
-    console.log(answers, __dirname);
-  })
-  .catch((error) => {
-    console.log("has error", error);
-  });
+console.log(getFiles(__dirname, []));
+
+// inquirer
+//   .prompt([
+//     {
+//       type: "list",
+//       name: "my list",
+//       message: "select from list",
+//       choices: ["react", "vue", "angular", "jquery", "svelte"],
+//     },
+//   ])
+//   .then((answers) => {
+//     console.log(answers);
+//   })
+//   .catch((error) => {
+//     console.log("has error", error);
+//   });
