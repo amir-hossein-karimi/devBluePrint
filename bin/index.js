27b0696cad181f3ef8b015d7bd67859b4cc47e33
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-09-10 01:54:31 -0700

    add __dirname and __filename

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,11 @@
 #! /usr/bin/env node
 
 import inquirer from "inquirer";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 inquirer
   .prompt([
@@ -12,7 +17,7 @@ inquirer
     },
   ])
   .then((answers) => {
-    console.log(answers);
+    console.log(answers, __dirname);
   })
   .catch((error) => {
     console.log("has error", error);
